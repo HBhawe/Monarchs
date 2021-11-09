74eef788246cd71f8129efd034029a252e9a93e6
HBhawe <harshalb97@outlook.com>
2021-11-09 12:10:53 +0100

    Adding script file

--- Extract_top1%.R
+++ Extract_top1%.R
@@ -1,1 +1,12 @@
-table = read.csv("...")
+table = read.csv("migrant_vs_resident_Final_stats.csv")
+test1 = table[order(-table$West_vs_resident_Fst), ]
+westvresident = test1[1:236, ]
+View(westvresident)
+test2 = table[order(-table$East_vs_resident_Fst), ]
+eastvresident = test2[1:236, ]
+test3 = table[order(-table$migrant_vs_resident_Fst), ]
+migrantvresident = test3[1:236, ]
+
+write.csv(westvresident, "West_vs_Resident.csv")
+write.csv(eastvresident, "East_vs_Resident.csv")
+write.csv(migrantvresident, "migrant_vs_Resident.csv")
